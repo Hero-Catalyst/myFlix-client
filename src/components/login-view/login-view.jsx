ca4eb0b21164425e48ed6d00cfcb0018ba4af70e
DistractedWanderer <smhall117@gmail.com>
2022-08-24 16:33:18 -0400

    login-view update
    uncommented props.onLoggedIn(username);

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,16 +1,20 @@
 //Hook
 import React, { useState } from 'react';
 
+//Component
 export function LoginView(props) {
+  //Calling the useState method with initial value(''), method returns destructured pair values
+  //Assigns current state value ('') to username, and assigns method that updates username to setUsername
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const handleSubmit = (e) => {
+    //(e) prevents the default refresh/change of the page from the handleSubmit() method
     e.preventDefault();
     console.log(username, password);
     /* Send a request to the server for authentication */
     /* then call props.onLoggedIn(username) */
-    // props.onLoggedIn(username);
+    props.onLoggedIn(username);  //this allows a user to be logged in regardless of credentials.
   };
 
   return (
@@ -28,7 +32,7 @@ export function LoginView(props) {
   );
 }
 
-/* code befor implementing the {useState} hook
+/* code before implementing the {useState} hook
 
 export class LoginView extends React.Component {
   constructor(props) {
