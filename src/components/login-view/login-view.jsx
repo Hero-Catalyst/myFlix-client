d211e4c4f330da49d9d7857d719129766677fcf5
DistractedWanderer <smhall117@gmail.com>
2022-08-24 16:24:24 -0400

    login-view update
    Updated code to implement useState hook from react

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,4 +1,34 @@
-import React from 'react';
+//Hook
+import React, { useState } from 'react';
+
+export function LoginView(props) {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password);
+    /* Send a request to the server for authentication */
+    /* then call props.onLoggedIn(username) */
+    // props.onLoggedIn(username);
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input type="text" value={username} onChange={e => setUsername(e.target.value)} />
+      </label>
+      <label>
+        Password:
+        <input type="password" value={password} onChange={e => setPassword(e.target.value)} />
+      </label>
+      <button type="submit" onClick={handleSubmit}>Submit</button>
+    </form>
+  );
+}
+
+/* code befor implementing the {useState} hook
 
 export class LoginView extends React.Component {
   constructor(props) {
@@ -30,7 +60,7 @@ export class LoginView extends React.Component {
     const { username, password } = this.state;
     console.log(username, password);
     /* Send a request to the server for authentication
-    then call this.props.onLoggedIn(username) */
+    then call this.props.onLoggedIn(username) 
     // this.props.onLoggedIn(usersname); 
   }
 
@@ -51,4 +81,4 @@ export class LoginView extends React.Component {
       </form>
     );
   }
-}
+} */
