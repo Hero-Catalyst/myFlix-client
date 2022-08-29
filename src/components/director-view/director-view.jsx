e93746544aa99fc0eb4cb15a7c7a7ff906765566
DistractedWanderer <smhall117@gmail.com>
2022-08-29 19:13:22 -0400

    director-view update
    Started adding code to make director-view functional

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
-import axios from 'axios';
+import { Card } from 'react-bootstrap';
 
 
 
 export class DirectorView extends React.Component {
+  render() {
+    const { movie, onBackClick } = this.props;
 
+    return (
+      <Card clssName="director-view">
+        <Card.Body>
+          <Card.Header>Director</Card.Header>
+          <Card.Title>{director.Name}</Card.Title>
+          <Card.Text>Bio: {director.Bio}</Card.Text>
+          <Card.Text>Born: {director.Birthday}</Card.Text>
+
+          <Button onClick={() => onBackClick()}>Back</Button>
+
+        </Card.Body>
+      </Card>
+    );
+  }
 
 }
