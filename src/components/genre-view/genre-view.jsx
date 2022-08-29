faddf4a35c476d4d5a4bcf36025622c3061efd44
DistractedWanderer <smhall117@gmail.com>
2022-08-29 19:47:06 -0400

    genre-view update
    adding code to allow pull of genre data and to make sure user has token

--- src/components/genre-view/genre-view.jsx
+++ src/components/genre-view/genre-view.jsx
@@ -4,6 +4,41 @@ import { Card } from 'react-bootstrap';
 
 
 export class GenreView extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      genres: [],
+      users: null
+    }
+  }
+
+  componentDidMount() {
+    let accessToken = localStorage.getItem('token');
+    if (accessToken !== null) {
+      this.setState({
+        user: localStorage.getItem('user')
+      });
+      this.getGenres(accessToken);
+    }
+  }
+
+  getGenres(token) {
+    axios.get('https://myflix-movieapi-76028.herokuapp.com/genres', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+        //Assign the result to the state
+        this.setState({
+          genres: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
+
+
+
   render() {
     const { movie, onBackClick } = this.props;
 
