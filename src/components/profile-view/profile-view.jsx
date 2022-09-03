c1446fbd438a2e6493776d3def2d92f09ee8159b
DistractedWanderer <smhall117@gmail.com>
2022-09-02 22:39:19 -0400

    update profile-view
    Playing around with code - added a form to make changes to profile - this will display, but I'm having trouble displaying the current user info, and making the necessary changes.

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -5,44 +5,106 @@ import axios from 'axios';
 import './profile-view.scss';
 
 
-export function ProfileView(props) {
-  const [user, setUser] = useState({
-    Username: props.username,
-    Password: props.password,
-    Email: props.email,
-    Birthday: props.birthday,
-    FavoriteMovies: []
-  });
+export function ProfileView() {
+  //HOOK useState
+  const [user, setUser] = useState("");
 
-  const favoriteMovieList = () => {
-
+  //METHODS
+  const getUser = (token) => {
+    axios.get('https://myflix-movieapi-76028.herokuapp.com/users/:Username', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+        //Assign the result to the state
+        this.setState({
+          Username: response.data.Username,
+          Password: response.data.Password,
+          Email: response.data.Email,
+          Birthday: response.data.Birthday,
+          FavoriteMovies: response.data.FavoriteMovies
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
-  const getUser = () => {
-
+  const favoriteMovieList = () => {
+    axios.get('https://myflix-movieapi-76028.herokuapp.com/users/:Username/movies', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+        //Assign the result to the state
+        this.setState({
+          FavoriteMovies: response.data.FavoriteMovies
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   const handleSubmit = (e) => {
+    e.preventDefault();
+  }
 
+  const handleUpdate = (e) => {
+    const name = e.user.Username;
+    setUser
   }
 
   const removeFav = (id) => {
     //function for remove favorite movie button
-  }
-
-  const handleUpdate = (e) => {
-    //function for rendering changes
-
   };
 
 
 
   return (
     <Container>
+
       <Row>
+        <Col>
+          <form onSubmit={handleSubmit}>
+            <h1>Need to make some changes?</h1>
 
+            <label>Change Username: </label>
+            <input
+              type="text"
+              name="username"
+              value={user.Username}
+              onChange={handleUpdate}
+            />
+            <Col>
+              <label>Change Password: </label>
+              <input
+                type="text"
+                name="password"
+                value={user.Password}
+                onChange={handleUpdate}
+              />
+            </Col>
+            <Col>
+              <label>Change Email: </label>
+              <input
+                type="email"
+                name="email"
+                value={user.Email}
+                onChange={handleUpdate}
+              />
+            </Col>
+            <Col>
+              <label>Change Birthday: </label>
+              <input
+                type="date"
+                name="birthday"
+                value={user.Birthday}
+                onChange={handleUpdate}
+              />
+            </Col>
+            <Button className="primary" type="submit">Submit</Button>
+          </form>
+        </Col>
       </Row>
-
-    </Container>
+    </Container >
   );
 }
