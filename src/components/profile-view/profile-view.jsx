6ad57edeef0fb426cd3bb4e7e7c9e1e96046806f
DistractedWanderer <smhall117@gmail.com>
2022-09-06 13:20:03 -0400

    Update profile-view
    Added 2 variables, altered methods accordingly, changed password form

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Container, Col, Row, Card, Button, Form } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
@@ -7,29 +7,30 @@ import './profile-view.scss';
 
 export function ProfileView() {
   //HOOK useState
-  const [user, setUser] = useState("");
+  const [user, setUser] = useState('');
+
+  const currentUser = localStorage.getItem('user');
+  const token = localStorage.getItem('token');
 
   //METHODS
-  const getUser = (token) => {
-    axios.get('https://myflix-movieapi-76028.herokuapp.com/users/:Username', {
-      headers: { Authorization: `Bearer ${token}` }
+  const getUser = (token, user) => {
+    axios.get('https://myflix-movieapi-76028.herokuapp.com/users/${user}', {
+      headers: { Authorization: `Bearer ${token}` },
     })
-      .then(response => {
+      .then((response) => {
         //Assign the result to the state
-
-        this.setState({
-          Username: response.data.Username,
-          Password: response.data.Password,
-          Email: response.data.Email,
-          Birthday: response.data.Birthday,
-          FavoriteMovies: response.data.FavoriteMovies
-        });
+        console.log(response.data);
+        setUser(response.data);
       })
       .catch(function (error) {
         console.log(error);
       });
 
-  }
+  };
+
+  useEffect(() => {
+    getUser(token, currentUser);
+  }, []);
 
   const favoriteMovieList = () => {
     axios.get('https://myflix-movieapi-76028.herokuapp.com/users/:Username/movies', {
@@ -79,9 +80,8 @@ export function ProfileView() {
             <Col>
               <label>Change Password: </label>
               <input
-                type="text"
+                type="password"
                 name="password"
-                value={user.Password}
                 onChange={handleUpdate}
               />
             </Col>
